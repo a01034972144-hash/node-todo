59369f8d6ea3ec3e4d9a34a3c3c9538cd97dc4ec
a01034972144-hash <a01034972144@gmail.com>
2026-03-13 23:00:41 +0900

    feat: add todo function

--- todo.js
+++ todo.js
@@ -18,3 +18,19 @@ function loadTodos() {
 function saveTodos(todos) {
     fs.writeFileSync('todos.json', JSON.stringify(todos, null, 2));
 }
+
+
+function add(content) {
+    const todos = loadTodos();
+
+    const newTodo = {
+        id: id(todos), 
+        content: content,
+        done: false
+    };
+
+    todos.push(newTodo);
+    saveTodos(todos);
+
+    console.log(`Todo가 추가되었습니다: ${content}`);
+}
