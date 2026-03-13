bf3248b06bd21648b25b36d786f39bd391492d64
a01034972144-hash <a01034972144@gmail.com>
2026-03-13 22:59:39 +0900

    feat: data load & save

--- todo.js
+++ todo.js
@@ -5,3 +5,16 @@ function id(todos) {
     if (todos.length === 0) return 1;
     return todos[todos.length - 1].id + 1;
 }
+
+function loadTodos() {
+    if (!fs.existsSync('todos.json')) {
+        fs.writeFileSync('todos.json', '[]');
+    }
+
+    const data = fs.readFileSync('todos.json', 'utf-8');
+    return JSON.parse(data);
+}
+
+function saveTodos(todos) {
+    fs.writeFileSync('todos.json', JSON.stringify(todos, null, 2));
+}
